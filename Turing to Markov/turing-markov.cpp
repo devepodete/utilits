8be6600f319e7af84ea961b5ce10ef564fff2ce4
Maksim Cheremisinov <54975008+devepodete@users.noreply.github.com>
2020-01-14 22:39:40 +0300

    Add files via upload

---
+++ Turing to Markov/turing-markov.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "./lib_source/processing.hpp"
+#include "./lib_source/output.hpp"
+
+int main(int argc, char *argv[]){
+	if(argc == 3){
+		proc::sep1 = argv[1];
+		proc::sep2 = argv[2];
+		std::cout << "Default state separator set by \'" << 
+			proc::sep1 << "\' and \'" << proc::sep2 << "\'" << std::endl;
+	}else if(argc == 1){
+		std::cout << "Default state separator set by \'(\' and \')\'" << std::endl;
+	}else if(argc != 1){
+		std::cout << "Usage: ./PROGRAM_NAME [STATE_SEPARATOR_1] [STATE_SEPARATOR_2]" << std::endl;
+		return 0;
+	}
+
+	std::vector<std::string> turing_commands;
+
+	std::string turing_command = "";
+	while(std::cin >> turing_command){
+		turing_commands.push_back(turing_command);
+	}
+
+	std::vector<std::string> markov_commands = convert(turing_commands);
+
+	for(int i = 0; i < markov_commands.size(); i++){
+		std::cout << markov_commands[i] << std::endl;
+	}
+
+	return 0;
+}
