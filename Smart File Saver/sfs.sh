ab62c4424575834f2324fa5e2d3d9da3956702b4
Maksim Cheremisinov <54975008+devepodete@users.noreply.github.com>
2020-05-13 12:16:34 +0300

    some code improvements

--- Smart File Saver/sfs.sh
+++ Smart File Saver/sfs.sh
@@ -1,81 +1,107 @@
 user_id=$(id -u)
-res="$(ls -A /run/user/${user_id}/gvfs)"
-if [ -z "$res" ]; then
-	echo "Device not found"
+device_name="$(ls -A /run/user/${user_id}/gvfs)"
+
+if [ -z "$device_name" ]; then
+    echo "Device not found"
 else
-	telegram_files=5
-	whatsapp_files=10
-	vk_files=10
+    telegram_files=5
+    whatsapp_files=10
+    vk_files=10
 
-	echo "Device found: ${res}"
+    echo "Device found: ${device_name}"
 
-	save_path="/home/zero/desktop/${res}"
-	mkdir ${save_path}
-	echo "Directory created: ${save_path}"
+    user_name=$(whoami)
 
-	device_path="/run/user/1000/gvfs/${res}"
+    save_path="/home/${user_name}/Desktop/${device_name}"
+    
+    #if directory does not exist
+    if [ ! -d "${save_path}" ]; then
+        mkdir ${save_path}
+    fi
 
-	cd ${device_path}
+    echo "Directory created: ${save_path}"
 
-	telegram_path=$(find . -maxdepth 3 -type d -name "Telegram")
-	whatsapp_path=$(find . -maxdepth 3 -type d -name "WhatsApp")
-	vk_path=$(find . -maxdepth 3 -type d -name "VK")
+    device_path="/run/user/1000/gvfs/${device_name}"
 
-	telegram_save_path=""
-	whatsapp_save_path=""
-	vk_save_path=""
+    echo "Device Path: ${device_path}"
+    cd ${device_path}
 
-	if ! [ -z "$telegram_path" ]; then
-		cd ${device_path}
-		echo "Telegram path: ${telegram_path}"
-		
-		telegram_save_path="${save_path}/Telegram"
-		mkdir $telegram_save_path
-		
-		cd "${telegram_path}"
+    telegram_path=$(find . -maxdepth 3 -type d -name "Telegram" | tail -n 1)
+    whatsapp_path=$(find . -maxdepth 3 -type d -name "WhatsApp" | tail -n 1)
+    vk_path=$(find . -maxdepth 3 -type d -name "VK" | tail -n 1)
 
-		find . -type f -name "*.jpg" | head -n ${telegram_files} | while read file_name
-		do
-			echo $file_name
-			cp "$file_name" "$telegram_save_path"
-		done
-	else
-		echo "Telegram path not found"
-	fi
-	
-	if ! [ -z "$whatsapp_path" ]; then
-		cd ${device_path}
-		echo "WhatsApp path: ${whatsapp_path}"
-		
-		whatsapp_save_path="${save_path}/WhatsApp"
-		mkdir $whatsapp_save_path
-		
-		cd "${whatsapp_path}"
+    telegram_save_path=""
+    whatsapp_save_path=""
+    vk_save_path=""
 
-		find . -type f -name "*.jpg" | head -n ${whatsapp_files} | while read file_name
-		do
-			echo $file_name
-			cp "$file_name" "$whatsapp_save_path"
-		done
-	else
-		echo "WhatsApp path not found"
-	fi
+    if ! [ -z "$telegram_path" ]; then
+        cd ${device_path}
+        echo "Telegram path: ${telegram_path}"
+        
+        telegram_save_path="${save_path}/Telegram"
+        
+        #if directory does not exist
+        if [ ! -d "${telegram_save_path}" ]; then
+            mkdir ${telegram_save_path}
+        fi
 
-	if ! [ -z "$vk_path" ]; then
-		cd ${device_path}
-		echo "VK path: ${vk_path}"
-		
-		vk_save_path="${save_path}/VK"
-		mkdir $vk_save_path
-		
-		cd "${vk_path}"
+        cd "${telegram_path}"
 
-		find . -type f -name "*.jpg" | head -n ${vk_files} | while read file_name
-		do
-			echo $file_name
-			cp "$file_name" "$vk_save_path"
-		done
-	else
-		echo "VK path not found"
-	fi
+        #find -P . -type f -name "*.jpg" -printf "%T@ %Tc %p\n" | sort -r | cut -d " " -f8 | head -n ${telegram_files} | while read file_name
+        find -P . -type f -name "*.jpg" | head -n ${telegram_files} | while read file_name
+        do
+            echo "Copying: ${file_name}"
+            cp "$file_name" "$telegram_save_path"
+        done
+    else
+        echo "Telegram path not found"
+    fi
+    
+    exit
+
+    if ! [ -z "$whatsapp_path" ]; then
+        cd ${device_path}
+        echo "WhatsApp path: ${whatsapp_path}"
+        
+        whatsapp_save_path="${save_path}/WhatsApp"
+        
+        #if directory does not exist
+        if [ ! -d "${whatsapp_save_path}" ]; then
+            mkdir ${whatsapp_save_path}
+        fi
+
+        cd "${whatsapp_path}"
+
+        #find -P . -type f -name "*.jpg" -printf "%T@ %Tc %p\n" | sort -r | cut -d " " -f8 | head -n ${whatsapp_files} | while read file_name
+        find -P . -type f -name "*.jpg" | head -n ${whatsapp_files} | while read file_name
+        do
+            echo "Copying: ${file_name}"
+            cp "$file_name" "$whatsapp_save_path"
+        done
+    else
+        echo "WhatsApp path not found"
+    fi
+
+    if ! [ -z "$vk_path" ]; then
+        cd ${device_path}
+        echo "VK path: ${vk_path}"
+        
+        vk_save_path="${save_path}/VK"
+        
+        #if directory does not exist
+        if [ ! -d "${vk_save_path}" ]; then
+            mkdir ${vk_save_path}
+        fi
+
+        cd "${vk_path}"
+
+        #find -P . -type f -name "*.jpg" -printf "%T@ %Tc %p\n" | sort -r | cut -d " " -f8 | head -n ${vk_files} | while read file_name
+        find -P . -type f -name "*.jpg" | head -n ${vk_files} | while read file_name
+        do
+            echo "Copying: ${file_name}"
+            cp "$file_name" "$vk_save_path"
+        done
+    else
+        echo "VK path not found"
+    fi
 fi
